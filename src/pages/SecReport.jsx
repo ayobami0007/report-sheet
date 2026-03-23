455c25a4c108bcd70b1a785dd620e8db14974eba
Sarat-12 <saratayobam@gmail.com>
2026-03-23 12:32:42 +0100

    downlaod btn added

--- src/pages/SecReport.jsx
+++ src/pages/SecReport.jsx
@@ -242,7 +242,7 @@ font-weight: 700 !important;
           <button
             onClick={addSubject}
             className="bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold px-4 py-2 rounded-lg shadow transition"
-          >]
+          >
             + Add Subject
           </button>
           <button
